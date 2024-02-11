a3df8140a7091a225d14dc0cd3001e326231973a
matheus1714 <matheussilvamartins1714@gmail.com>
2024-02-11 12:31:38 -0300

    add socket and redis

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from 'fastify'
 import { prisma } from '../../lib/prisma'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
+import { redis } from '../../lib/redis'
+import { voting } from '../../utils/voting-pub-sub'
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -34,6 +36,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                         id: userPreviusVoteOnPoll.id
                     }
                 })
+
+                const votes = await redis.zincrby(pollId, -1, userPreviusVoteOnPoll.pollOptionId)
+
+                voting.publish(pollId, {
+                    pollOptionId: userPreviusVoteOnPoll.pollOptionId,
+                    votes: Number(votes),
+                })
             }else if(userPreviusVoteOnPoll){
                 return reply.status(400).send({ message: 'You already voted on this poll' })
             }
@@ -56,6 +65,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                 pollId,
                 pollOptionId
             }
+        })
+
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
         })
 
         return reply.status(201).send()
